d2d051a25eff91468a23452ea888780d7f32d8c6
gfrey <gfreycode@gmail.com>
2024-03-07 10:06:09 -0500

    Update home.py with new routes and parameters

--- src/app/api/routes/home.py
+++ src/app/api/routes/home.py
@@ -3,5 +3,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/")
-def hello_world():
-    return {"message": "Hello World"}
+def hello_world(username: str = "World"):
+    return {"message": f"Hello {username}"}
+
+@router.get("/{id}")
+def get_item(id: int):
+    id = int(id)
+    return { "id": id }
