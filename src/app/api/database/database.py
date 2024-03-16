29bd87497b41719a7e974c50bc211c7983fba0d6
gfrey <gfreycode@gmail.com>
2024-03-16 07:19:54 -0500

    Remove hardcoded database URL

--- src/app/api/database/database.py
+++ src/app/api/database/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-## SQLALCHEMY_DATABASE_URL = "postgresql://fl0user:Jlep1h5PWZoM@ep-late-truth-a5wu5gad.us-east-2.aws.neon.fl0.io:5432/database?sslmode=require"
 SQLALCHEMY_DATABASE_URL = os.environ.get('SQLALCHEMY_DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
