77aa424161439ce06e27d1613d502d8385c0a766
gfrey <gfreycode@gmail.com>
2024-03-23 14:33:17 -0500

    Update global CSS and add new components

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,20 @@
 import { Header } from "@/components/Header"
+import AddIcon from '@mui/icons-material/Add';
 
 const HomePage: React.FC = () => {
+    const [firstname, lastname] = ['John', 'Doe']
     return (
         <>
             <Header />
+            <main className=" p-8 flex flex-col gap-4">
+                <h1 className=" font-bold text-4xl">Hello😊!! {firstname} {lastname}.</h1>
+                <div className="w-full flex justify-end">
+                    <button className="flex gap-2 px-6 py-4 bg-gray-800 w-fit h-fit rounded-full justify-center text-white">
+                        Create New Todochallenge
+                        <AddIcon />
+                    </button>
+                </div>
+            </main>
         </>
     )
 }
