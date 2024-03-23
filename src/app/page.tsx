676e853453cea68b6c06506483aaacf7f3474e69
gfrey <gfreycode@gmail.com>
2024-03-23 14:09:25 -0500

    Add global CSS class and create home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Header } from "@/components/header";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Link href="/home" className="flex gap-2 p-2 bg-gray-800 w-fit h-fit rounded-md justify-center text-white">
+        Vamos!
+      </Link>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Header: React.FC = () => {
     return (
-        <header className="flex justify-between p-8">
-            <div className='flex gap-2 p-2 bg-gray-800 w-fit h-fit rounded-md justify-center border-slate-400 border-[0.5px] border-double'>
+        <header className="flex justify-between p-8 w-full h-fit">
+            <Link href="/" className='flex gap-2 p-2 bg-gray-800 w-fit h-fit rounded-md justify-center text-white' title='Go to landing'>
                 <FormatListBulletedIcon />
                 <h3>Todochallenge</h3>
-            </div>
-            <figure className='w-11 h-11 overflow-hidden rounded-full cursor-pointer'>
+            </Link>
+            <figure className='w-11 h-11 overflow-hidden rounded-full cursor-pointer border-4 border-gray-800'>
                 <Image src="/assets/images/user_default.jpeg" alt="profile" width={415} height={415} />
             </figure>
         </header>
